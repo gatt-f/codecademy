ff796534b0929965cbb088f1f5536a9825d22f3d
Franz Gatt <franz.gatt@student.uibk.ac.at>
2022-04-01 17:16:28 +0200

    main.js erstellt und Code aus codecademy implementiert

--- js_introduction/app.js
+++ js_introduction/app.js
@@ -1,3 +1,4 @@
+//Workload 3 - Franz Gatt
 console.log(Math.floor(Math.random()*100))
 console.log(Math.ceil(43.8));
 console.log(Number.isInteger(2017));
---
+++ js_introduction/main.js
@@ -0,0 +1,6 @@
+//Workload 3 - Franz Gatt
+let newVariable = 'Playing around with typeof.';
+
+console.log(typeof newVariable);
+newVariable = 1;
+console.log(typeof newVariable);
